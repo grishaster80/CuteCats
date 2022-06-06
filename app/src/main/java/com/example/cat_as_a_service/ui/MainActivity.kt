d9feb1a2d8da02a69d394c6f46a13edb318d2cd5
Grigory Shimichev <grishaster80@gmail.com>
2022-06-07 00:50:10 +0700

    progress bar added

--- app/src/main/java/com/example/cat_as_a_service/ui/MainActivity.kt
+++ app/src/main/java/com/example/cat_as_a_service/ui/MainActivity.kt
@@ -6,25 +6,23 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.magnifier
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.ImageLoader
 import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
 import com.example.cat_as_a_service.MyApplication
 import com.example.cat_as_a_service.R
 import com.example.cat_as_a_service.network.NetworkConstants
@@ -62,8 +60,18 @@ fun Greeting(name: String) {
 
 @Composable
 fun CuteCatsScreen() {
+
     val randomNumber = remember {
         mutableStateOf(0)
+    }
+    val isImageLoaded = remember {
+        mutableStateOf(false)
+    }
+    if (!isImageLoaded.value) {
+        Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center) {
+            CircularProgressIndicator()
+        }
     }
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -73,6 +81,16 @@ fun CuteCatsScreen() {
         AsyncImage(
             model = "${NetworkConstants.BASE_URL}${NetworkConstants.RANDOM_CAT}?${randomNumber.value}",
             contentDescription = "Cat picture",
+            onState = {
+                when(it){
+                    is AsyncImagePainter.State.Loading -> {
+                        isImageLoaded.value = false
+                    }
+                    is AsyncImagePainter.State.Success -> {
+                        isImageLoaded.value = true
+                    }
+                }
+            }
         )
         Button(
             modifier = Modifier.padding(vertical = 24.dp),
